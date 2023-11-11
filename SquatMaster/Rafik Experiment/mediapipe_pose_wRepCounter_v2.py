a7521a6cd509e25e1f973a147ebf02d336d230fb
Rafik1001 <rafik.babayev01@gmail.com>
2023-11-11 15:42:59 +0100

    Added joint angle calculator and stage classifier python codes Added a new clipped video of a male squatting

--- SquatMaster/Rafik Experiment/mediapipe_pose_wRepCounter_v2.py
+++ SquatMaster/Rafik Experiment/mediapipe_pose_wRepCounter_v2.py
@@ -4,22 +4,10 @@ import numpy as np
 import sys
 sys.path.insert(0,'../functions')
 from rep_counter1 import rep_counter
+from joint_angle_calc1 import calculate_angle
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 import os
-
-# Define a function that calculates an angle between three points in x,y space
-def calculate_angle(a,b,c):
-    a = np.array(a) # First
-    b = np.array(b) # Mid
-    c = np.array(c) # End
-    
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-    
-    if angle >180.0:
-        angle = 360-angle
-    return angle 
 
 
 # Specify the location of the file with the video to be read
