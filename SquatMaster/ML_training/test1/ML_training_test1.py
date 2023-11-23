b73e8d3ed06a0b7f42546efd4f6efcd5cd88334c
Rafik1001 <rafik.babayev01@gmail.com>
2023-11-23 23:37:49 +0100

    Added a folder for ML training test for classification with the run script

---
+++ SquatMaster/ML_training/test1/ML_training_test1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import pathlib
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos.tar', origin=dataset_url, extract=True)
+data_dir = pathlib.Path(data_dir).with_suffix('')
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+
